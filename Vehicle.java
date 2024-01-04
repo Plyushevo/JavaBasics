af8667015ab0339cc826fdd2c6fabb6f4ba42c57
Кирилл Бельков <plyushevo@gmail.com>
2024-01-04 14:19:19 +0200

    chapter 4 end

--- Vehicle.java
+++ Vehicle.java
@@ -2,6 +2,13 @@ class Vehicle {
   int passengers; 
   int fuelcap;
   int mpg;
+
+  Vehicle(int p, int f, int m) {
+    passengers = p;
+    fuelcap = f;
+    mpg = m;
+  }
+
   int range() {
     return fuelcap * mpg;
   }
@@ -11,24 +18,13 @@ class Vehicle {
   }
 } 
 
-/**
- * AddMeth
- */
-class CompFuel {
+class VehConsDemo {
 
   public static void main(String[] args) {
-    Vehicle minivan = new Vehicle();
-    Vehicle sportcar = new Vehicle();
+    Vehicle minivan = new Vehicle(7, 16, 21);
+    Vehicle sportcar = new Vehicle(2, 14, 12);
     double gallons;
     int distance = 252;
-
-    minivan.passengers = 7;
-    minivan.fuelcap = 16;
-    minivan.mpg = 21;
-
-    sportcar.passengers = 2;
-    sportcar.fuelcap = 14;
-    sportcar.mpg = 12;
 
     gallons = minivan.fuelNeeded(distance);
 
