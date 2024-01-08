6798d2179a9ef6fc399bff5e79b98904f4a8ef80
plyushevo <plyushevo@gmail.com>
2024-01-08 17:28:18 +0200

    final chapter task

---
+++ src/com/javaForBeginners/chapter5array/tasks/TwoDArray.java
@@ -0,0 +1,10 @@
+package src.com.javaForBeginners.chapter5array.tasks;
+
+public class TwoDArray {
+  double[] x = new double[10];
+  double y[] = new double[10];
+
+  int[] a = {1, 2, 3, 4, 5};
+  
+
+}
